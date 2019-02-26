b71cbd8ae0704a2a3ca3e763fba6abbb06072350
Magnus Haukeland <armonh@stud.ntnu.no>
2019-02-26 11:58:11 +0100

    accelerometer

---
+++ 4_accelerometer/uart.h
@@ -0,0 +1,53 @@
+#include <stdint.h>
+#define UART ((NRF_UART_REG*)0x40002000)
+
+typedef struct {
+	//Task
+	volatile uint32_t STARTRX;
+	volatile uint32_t STOPRX;
+	volatile uint32_t STARTTX;
+	volatile uint32_t STOPTX;
+	volatile uint32_t reserved_0[3];
+	volatile uint32_t SUSPEND;
+	//Events
+	volatile uint32_t reserved_1[56];
+	volatile uint32_t CTS;
+	volatile uint32_t NCTS;
+	volatile uint32_t RXDRDY;
+	volatile uint32_t reserved_2[4];
+	volatile uint32_t TXDRDY;
+	volatile uint32_t reserved_3[1];
+	volatile uint32_t ERROR;
+	volatile uint32_t reserved_4[7];
+	volatile uint32_t RXTO;
+
+	volatile uint32_t reserved_5[110];
+	//Registers
+	volatile uint32_t INTEN;
+	volatile uint32_t INTENSET;
+	volatile uint32_t INTENCLR;
+	volatile uint32_t reserved_6[93];
+	volatile uint32_t ERRORSRC;
+	volatile uint32_t reserved_7[31];
+
+	volatile uint32_t ENABLE;
+	volatile uint32_t reserved_8[1];
+	volatile uint32_t PSELRTS;
+	volatile uint32_t PSELTXD;
+	volatile uint32_t PSELCTS;
+	volatile uint32_t PSELRXD;
+
+	volatile uint32_t RXD;
+	volatile uint32_t TXD;
+	volatile uint32_t reserved_9[1];
+	volatile uint32_t BAUDRATE;
+	volatile uint32_t reserved_10[17];
+	volatile uint32_t CONFIG;
+
+
+
+} NRF_UART_REG;
+
+void uart_init();
+void uart_send(char letter);
+char uart_read();
